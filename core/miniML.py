bugfix: y unit was loaded incorrectly (from heka files) if not specified by user, leading to an error. Changed default of scaling from 1e12 to 1.

core/miniML.py
@@ -197,7 +197,7 @@ class MiniTrace():
 
     @classmethod
     def from_heka_file(cls, filename: str, rectype: str, group: int=1, exclude_series:list=[], exclude_sweeps:dict={},
-                        scaling: float=1e12, unit: str=None, resample: bool=True) -> MiniTrace:
+                        scaling: float=1, unit: str=None, resample: bool=True) -> MiniTrace:
         ''' Loads data from a HEKA .dat file. Name of the PGF sequence needs to be specified.
 
         Parameters
@@ -278,7 +278,7 @@ class MiniTrace():
             else:
                 data = np.append(data, dat[0])
         
-        data_unit = unit if unit is not None else bundle.pul[group][series[0]][0][0].YUnit
+        data_unit = unit if unit is not None else bundle.pul[group][series_list[0]][0][0].YUnit
 
         MiniTrace.excluded_sweeps = exclude_sweeps
         MiniTrace.exlucded_series = exclude_series